7bd68e3646c704913523baf6f96c77814842d510
aisuluuos <167067537+aisuluuos@users.noreply.github.com>
2024-05-24 03:23:07 +0600

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,29 +1,9 @@
 const API = "http://localhost:8000/menu";
+const searchInput = document.getElementById("search-input");
+//!--------------------SEARCH-------------------------
 document.addEventListener("DOMContentLoaded", function () {
-  const cartLink = document.getElementById("cart-link");
-  const modal = document.getElementById("cart-modal");
-  const closeButton = document.querySelector(".close-button");
-
-  cartLink.addEventListener("click", function (event) {
-    event.preventDefault();
-    modal.style.display = "block";
-  });
-
-  closeButton.addEventListener("click", function () {
-    modal.style.display = "none";
-  });
-
-  window.addEventListener("click", function (event) {
-    if (event.target === modal) {
-      modal.style.display = "none";
-    }
-  });
-
-  const searchButton = document.getElementById("search-button");
-  const searchInput = document.getElementById("search-input");
-
-  searchButton.addEventListener("click", function () {
-    const query = searchInput.value;
-    alert("Вы ищете: " + query);
+  searchInput.addEventListener("input", function (e) {
+    searchValue = e.target.value;
+    readFood();
   });
 });
