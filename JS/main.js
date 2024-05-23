7172755408f0520c94b1f4c9f48ce5dd8fe8b50f
nnurzat <n6228802@gmai.com>
2024-05-24 01:41:14 +0600

    navbar

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,29 @@
 const API = "http://localhost:8000/menu";
-document.getElementById("navbar-toggle").addEventListener("click", function () {
-  const navbarMenu = document.querySelector(".navbar-menu");
-  navbarMenu.classList.toggle("active");
+document.addEventListener("DOMContentLoaded", function () {
+  const cartLink = document.getElementById("cart-link");
+  const modal = document.getElementById("cart-modal");
+  const closeButton = document.querySelector(".close-button");
+
+  cartLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    modal.style.display = "block";
+  });
+
+  closeButton.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+
+  const searchButton = document.getElementById("search-button");
+  const searchInput = document.getElementById("search-input");
+
+  searchButton.addEventListener("click", function () {
+    const query = searchInput.value;
+    alert("Вы ищете: " + query);
+  });
 });
