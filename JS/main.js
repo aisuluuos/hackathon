8bd4c5287cf361637be5d8ca8c8be8c041e8922e
nnurzat <n6228802@gmai.com>
2024-05-23 14:38:54 +0600

    half

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,5 @@
 const API = "http://localhost:8000/menu";
+document.getElementById("navbar-toggle").addEventListener("click", function () {
+  const navbarMenu = document.querySelector(".navbar-menu");
+  navbarMenu.classList.toggle("active");
+});
